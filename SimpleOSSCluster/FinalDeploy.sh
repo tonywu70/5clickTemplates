6328c74ba7ed809f2eaeb2c2c9bb368a0fd5b1e2
Taylor Newill <tanewill@microsoft.com>
2016-05-04 09:00:52 -0400

    Update FinalDeploy.sh

--- SimpleOSSCluster/FinalDeploy.sh
+++ SimpleOSSCluster/FinalDeploy.sh
@@ -3,7 +3,7 @@ mkdir -p /home/azureuser/bin/
 cd /home/azureuser/bin
 
 #download utilites and the HeadComputeNodeDeploy script
-wget https://raw.githubusercontent.com/tanewill/sandbox/Ubuntu/DeployMultiVM/HeadComputeNodeDeploy.sh
+wget https://raw.githubusercontent.com/tanewill/5clickTemplates/SurfCluster/SimpleOSSCluster/HeadComputeNodeDeploy.sh
 wget https://raw.githubusercontent.com/tanewill/utils/master/authMe.sh
 wget https://raw.githubusercontent.com/tanewill/utils/master/myClusRun.sh
 chmod +x *
