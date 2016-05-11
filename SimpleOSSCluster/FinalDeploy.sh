50075406e5c260620254240bcb68154b06cc9c5e
Noesis-Support <support@noesissolutions.com>
2016-05-11 11:17:07 +0000

    Update FinalDeploy.sh

--- SimpleOSSCluster/FinalDeploy.sh
+++ SimpleOSSCluster/FinalDeploy.sh
@@ -3,9 +3,9 @@ mkdir -p /home/azureuser/bin/
 cd /home/azureuser/bin
 
 #download utilites and the HeadComputeNodeDeploy script
-wget https://raw.githubusercontent.com/tanewill/5clickTemplates/SurfCluster/SimpleOSSCluster/HeadComputeNodeDeploy.sh
-wget https://raw.githubusercontent.com/tanewill/utils/master/authMe.sh
-wget https://raw.githubusercontent.com/tanewill/utils/master/myClusRun.sh
+wget https://raw.githubusercontent.com/Noesis-Support/5clickTemplates/SurfCluster/SimpleOSSCluster/HeadComputeNodeDeploy.sh
+wget https://raw.githubusercontent.com/Noesis-Support/utils/master/authMe.sh
+wget https://raw.githubusercontent.com/Noesis-Support/utils/master/myClusRun.sh
 chmod +x *
 
 #Run HeadComputeNodeDeploy script
