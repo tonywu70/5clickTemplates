98fb3363117794a78ea9850d874bdb7309bd7666
Taylor Newill <tanewill@microsoft.com>
2016-05-26 07:02:19 -0400

    Update FinalDeploy.sh

--- SimpleOSSCluster/FinalDeploy.sh
+++ SimpleOSSCluster/FinalDeploy.sh
@@ -6,6 +6,8 @@ cd /home/azureuser/bin
 wget https://raw.githubusercontent.com/Noesis-Support/5clickTemplates/SurfCluster/SimpleOSSCluster/HeadComputeNodeDeploy.sh
 wget https://raw.githubusercontent.com/Noesis-Support/utils/master/authMe.sh
 wget https://raw.githubusercontent.com/Noesis-Support/utils/master/myClusRun.sh
+wget https://aka.ms/ubuntuopenfoam_nonhpc
+tar -xzf ubuntuopenfoam_nonhpc -C /opt
 chmod +x *
 
 #Run HeadComputeNodeDeploy script
