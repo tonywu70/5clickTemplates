d864c3f7c0fc7f2a220f98d2cda767b52e4d463b
Taylor Newill <tanewill@microsoft.com>
2016-05-26 07:15:28 -0400

    Update FinalDeploy.sh

--- SimpleOSSCluster/FinalDeploy.sh
+++ SimpleOSSCluster/FinalDeploy.sh
@@ -8,6 +8,7 @@ cd /home/azureuser/bin
 #wget https://raw.githubusercontent.com/Noesis-Support/utils/master/myClusRun.sh
 wget https://aka.ms/ubuntuopenfoam_nonhpc
 tar -xzf ubuntuopenfoam_nonhpc -C /opt
+runuser -l azureuser -c "bin/myClusRun.sh 'echo \"source /opt/openfoam30/etc/bashrc\">>.bashrc'"
 #chmod +x *
 
 #Run HeadComputeNodeDeploy script
