d28f269a44add880a9b3b28fcf63411878bfe6e3
Taylor Newill <tanewill@microsoft.com>
2016-09-13 09:01:43 -0400

    Update cn-setup.sh

--- RawCluster/cn-setup.sh
+++ RawCluster/cn-setup.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-touch myFirstLocation
-touch /home/mySecondLocation
+sudo yum install -y -q nfs-utils
+mkdir -p /mnt/nfsshare
+systemctl enable rpcbind
+systemctl enable nfs-server
+systemctl enable nfs-lock
+systemctl enable nfs-idmap
+systemctl start rpcbind
+systemctl start nfs-server
+systemctl start nfs-lock
+systemctl start nfs-idmap
+localip=`hostname -i | cut --delimiter='.' -f -3`
+#echo "10.2.1.7:/mnt/nfsshare    /mnt/nfsshare   nfs defaults 0 0" | sudo tee -a /etc/fstab
+#mount -a
