bc0f0b79268045ac14d13f3080fc783349673bac
Taylor Newill <tanewill@microsoft.com>
2016-09-13 09:38:38 -0400

    Update cn-setup.sh

--- RawCluster/cn-setup.sh
+++ RawCluster/cn-setup.sh
@@ -10,5 +10,5 @@ systemctl start nfs-server
 systemctl start nfs-lock
 systemctl start nfs-idmap
 localip=`hostname -i | cut --delimiter='.' -f -3`
-#echo "10.2.1.7:/mnt/nfsshare    /mnt/nfsshare   nfs defaults 0 0" | tee -a /etc/fstab
-#mount -a
+echo "10.2.1.7:/mnt/nfsshare    /mnt/nfsshare   nfs defaults 0 0" | tee -a /etc/fstab
+mount -a
