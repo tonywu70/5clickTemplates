84328b5f50bfffcaf39b85d08ecd261d786f6322
Taylor Newill <tanewill@microsoft.com>
2016-09-13 09:37:09 -0400

    Update hn-setup.sh

--- RawCluster/hn-setup.sh
+++ RawCluster/hn-setup.sh
@@ -12,5 +12,5 @@ systemctl start nfs-server
 systemctl start nfs-lock
 systemctl start nfs-idmap
 localip=`hostname -i | cut --delimiter='.' -f -3`
-echo "/mnt/nfsshare $localip.*(rw,sync,hard,intr,no_root_squash,no_all_squash)" | tee -a /etc/exports
+echo "/mnt/nfsshare $localip.*(rw,sync,no_root_squash,no_all_squash)" | tee -a /etc/exports
 systemctl restart nfs-server
