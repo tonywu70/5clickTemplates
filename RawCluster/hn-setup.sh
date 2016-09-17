c9845f8674d70c3ebed423173b3dab17e0c2f635
Taylor Newill <tanewill@microsoft.com>
2016-09-17 19:05:18 -0400

    Update hn-setup.sh

--- RawCluster/hn-setup.sh
+++ RawCluster/hn-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+USER=$1
 wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
 rpm -ivh epel-release-7-8.noarch.rpm
 
@@ -16,7 +17,7 @@ systemctl start nfs-idmap
 localip=`hostname -i | cut --delimiter='.' -f -3`
 echo "/mnt/nfsshare $localip.*(rw,sync,no_root_squash,no_all_squash)" | tee -a /etc/exports
 systemctl restart nfs-server
-
-nmap -sn $localip.* | grep $localip. | awk '{print $5}' > nodeips.txt
+mv passwordlessAuth.sh /home/$USER/bin/
+nmap -sn $localip.* | grep $localip. | awk '{print $5}' > /home/$USER/bin/nodeips.txt
 
 
