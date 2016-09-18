ed164d098d42276fa542221fb98385ee5af6574a
Taylor Newill <tanewill@microsoft.com>
2016-09-17 20:42:14 -0400

    Update hn-setup.sh

--- RawCluster/hn-setup.sh
+++ RawCluster/hn-setup.sh
@@ -4,6 +4,7 @@ wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
 rpm -ivh epel-release-7-8.noarch.rpm
 
 yum install -y -q nfs-utils sshpass nmap
+yum groupinstall -y "X Window System"
 mkdir -p /mnt/nfsshare
 chmod -R 777 /mnt/nfsshare/
 systemctl enable rpcbind
