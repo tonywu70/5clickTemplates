d946cb5ba943554072cab71e5725ecc6bd0096cf
Taylor Newill <tanewill@microsoft.com>
2016-09-17 20:28:28 -0400

    Update hn-setup.sh

--- RawCluster/hn-setup.sh
+++ RawCluster/hn-setup.sh
@@ -14,14 +14,13 @@ systemctl start rpcbind
 systemctl start nfs-server
 systemctl start nfs-lock
 systemctl start nfs-idmap
-localip=`hostname -i | cut --delimiter='.' -f -3`
-echo "/mnt/nfsshare $localip.*(rw,sync,no_root_squash,no_all_squash)" | tee -a /etc/exports
-systemctl restart nfs-server
-mv passwordlessAuth.sh /home/$USER/bin/
-nmap -sn $localip.* | grep $localip. | awk '{print $5}' > /home/$USER/bin/nodeips.txt
 
 wget https://raw.githubusercontent.com/tanewill/5clickTemplates/master/RawCluster/install-fluent.sh
 chmod +x install-fluent.sh
 source install-fluent.sh
 
-
+localip=`hostname -i | cut --delimiter='.' -f -3`
+echo "/mnt/nfsshare $localip.*(rw,sync,no_root_squash,no_all_squash)" | tee -a /etc/exports
+systemctl restart nfs-server
+mv passwordlessAuth.sh /home/$USER/bin/
+nmap -sn $localip.* | grep $localip. | awk '{print $5}' > /home/$USER/bin/nodeips.txt
