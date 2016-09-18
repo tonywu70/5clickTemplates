d790d8d730b2dead87f61e05091b81d9a3f6135a
Taylor Newill <tanewill@microsoft.com>
2016-09-17 21:14:31 -0400

    Update install-fluent.sh

--- RawCluster/install-fluent.sh
+++ RawCluster/install-fluent.sh
@@ -1,9 +1,11 @@
-mkdir /mnt/ANSYS1
-mkdir /mnt/ANSYS2
-mkdir /mnt/ANSYS3
 mkdir /mnt/resource/INSTALLERS
 
-wget http://azbenchmarkstorage.blob.core.windows.net/ansysbenchmarkstorage/ANSYS.tgz -O /mnt/resource/ANSYS.tgz
+wget --quiet http://azbenchmarkstorage.blob.core.windows.net/ansysbenchmarkstorage/ANSYS.tgz -O /mnt/resource/ANSYS.tgz
 tar -xzf /mnt/resource/ANSYS.tgz -C /mnt/resource/INSTALLERS/
 
+cd /mnt/resource/INSTALLERS/ANSYS/
 source /mnt/resource/INSTALLERS/ANSYS/INSTALL -silent -install_dir "/mnt/nfsshare/ansys_inc/" -fluent
+
+echo SERVER=1055@52.169.161.205 > /mnt/nfsshare/ansys_inc/shared_files/licensing/ansyslmd.ini
+echo ANSYSLI_SERVERS=2325@52.169.161.205 >> /mnt/nfsshare/ansys_inc/shared_files/licensing/ansyslmd.ini
+
