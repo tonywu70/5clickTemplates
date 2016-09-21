4d57d7c8a4175cf060bb91b6254c2ced32af9f54
Taylor Newill <tanewill@microsoft.com>
2016-09-21 10:51:00 -0700

    Update install-fluent.sh

--- RawCluster/install-fluent.sh
+++ RawCluster/install-fluent.sh
@@ -15,7 +15,7 @@ echo export FLUENT_HOSTNAME=$HOST >> /home/$USER/.bashrc
 echo export INTELMPI_ROOT=/opt/intel/impi/5.1.3.181 >> /home/$USER/.bashrc
 echo export I_MPI_FABRICS=shm:dapl >> /home/$USER/.bashrc
 echo export I_MPI_DAPL_PROVIDER=ofa-v2-ib0 >> /home/$USER/.bashrc
-echo export PATH=/mnt/scratch/ansys_inc/v172/fluent/bin:/opt/intel/impi/5.1.3.181/bin64:$PATH >> /home/$USER/.bashrc
+echo export PATH=/mnt/nfsshare/ansys_inc/v172/fluent/bin:/opt/intel/impi/5.1.3.181/bin64:$PATH >> /home/$USER/.bashrc
 echo export I_MPI_DYNAMIC_CONNECTION=0 >> /home/$USER/.bashrc
 
 source /mnt/resource/INSTALLERS/ANSYS/INSTALL -silent -install_dir "/mnt/nfsshare/ansys_inc/" -fluent
