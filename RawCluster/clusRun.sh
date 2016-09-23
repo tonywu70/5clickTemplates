c4ac85da55008d71dd6518afe18e0f736103f7eb
Taylor Newill <tanewill@microsoft.com>
2016-09-23 07:49:43 -0700

    Update clusRun.sh

--- RawCluster/clusRun.sh
+++ RawCluster/clusRun.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
-NAMES=`cat ~/bin/nodenames.txt` #names from names.txt file
-echo "launching $1"
+USER=$1
+NAMES=`cat /home/$USER/bin/nodenames.txt` #names from names.txt file
+echo "launching $2"
 for NAME in $NAMES; do
   #the & here will fork off a run for each node and move to the next, the wait at the end waits until all is complete
   echo $NAME
-  ssh -t $NAME $1
+  ssh -t $NAME $2
 done
 wait
-echo "completed $1"
+echo "completed $2"
