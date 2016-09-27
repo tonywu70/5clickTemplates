8b7e9d7e223490d87572adc72614756dca86cda3
Taylor Newill <tanewill@microsoft.com>
2016-09-26 21:19:31 -0400

    Update hn-setup.sh

--- RawCluster/hn-setup.sh
+++ RawCluster/hn-setup.sh
@@ -10,6 +10,7 @@ rpm -ivh epel-release-7-8.noarch.rpm
 yum install -y -q nfs-utils sshpass nmap
 yum groupinstall -y "X Window System"
 mkdir -p /mnt/nfsshare
+localip=`hostname -i | cut --delimiter='.' -f -3`
 echo "/mnt/nfsshare $localip.*(rw,sync,no_root_squash,no_all_squash)" | tee -a /etc/exports
 chmod -R 777 /mnt/nfsshare/
 systemctl enable rpcbind
@@ -36,8 +37,6 @@ chmod +x /home/$USER/bin/clusRun.sh
 chmod +x /home/$USER/bin/cn-setup.sh
 chown $USER:$USER /home/$USER/bin/*
 
-localip=`hostname -i | cut --delimiter='.' -f -3`
-mv passwordlessAuth.sh /home/$USER/bin/
 nmap -sn $localip.* | grep $localip. | awk '{print $5}' > /home/$USER/bin/nodeips.txt
 myhost=`hostname -i`
 sed -i '/'$myhost'/d' /home/$USER/bin/nodeips.txt
