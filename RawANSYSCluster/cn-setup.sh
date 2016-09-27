3de86d68ac95fa5ee1480a635c13788bdc3a5770
Taylor Newill <tanewill@microsoft.com>
2016-09-26 23:22:08 -0400

    Add files via upload

---
+++ RawANSYSCluster/cn-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+IPPRE=$1
+yum install -y -q nfs-utils
+mkdir -p /mnt/nfsshare
+chmod 777 /mnt/nfsshare
+systemctl enable rpcbind
+systemctl enable nfs-server
+systemctl enable nfs-lock
+systemctl enable nfs-idmap
+systemctl start rpcbind
+systemctl start nfs-server
+systemctl start nfs-lock
+systemctl start nfs-idmap
+localip=`hostname -i | cut --delimiter='.' -f -3`
+echo "$IPPRE:/mnt/nfsshare    /mnt/nfsshare   nfs defaults 0 0" | tee -a /etc/fstab
+showmount -e 10.0.0.4
+mount -a
+
+ln -s /opt/intel/impi/5.1.3.181/intel64/bin/ /opt/intel/impi/5.1.3.181/bin
+ln -s /opt/intel/impi/5.1.3.181/lib64/ /opt/intel/impi/5.1.3.181/lib
+chown -R azureuser:azureuser /mnt/resource/
+
+df
---
+++ RawANSYSCluster/install-fluent.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+USER=$1
+HOST=`hostname`
+
+mkdir /mnt/resource/INSTALLERS
+tar -xzf /mnt/resource/ANSYS.tgz -C /mnt/resource/INSTALLERS/
+
+cd /mnt/resource/INSTALLERS/ANSYS/
+mkdir -p /mnt/nfsshare/ansys_inc/shared_files/licensing/
+
+echo SERVER=1055@52.169.161.205 > /mnt/nfsshare/ansys_inc/shared_files/licensing/ansyslmd.ini
+echo ANSYSLI_SERVERS=2325@52.169.161.205 >> /mnt/nfsshare/ansys_inc/shared_files/licensing/ansyslmd.ini
+
+echo export FLUENT_HOSTNAME=$HOST >> /home/$USER/.bashrc
+echo export INTELMPI_ROOT=/opt/intel/impi/5.1.3.181 >> /home/$USER/.bashrc
+echo export I_MPI_FABRICS=shm:dapl >> /home/$USER/.bashrc
+echo export I_MPI_DAPL_PROVIDER=ofa-v2-ib0 >> /home/$USER/.bashrc
+echo export I_MPI_ROOT=/opt/intel/compilers_and_libraries_2016.2.181/linux/mpi >> /home/$USER/.bashrc
+echo export PATH=/mnt/nfsshare/ansys_inc/v172/fluent/bin:/opt/intel/impi/5.1.3.181/bin64:$PATH >> /home/$USER/.bashrc
+echo export I_MPI_DYNAMIC_CONNECTION=0 >> /home/$USER/.bashrc
+
+source /mnt/resource/INSTALLERS/ANSYS/INSTALL -silent -install_dir "/mnt/nfsshare/ansys_inc/" -fluent
+
+
+
