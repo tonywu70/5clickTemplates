b970a52108f15a4fa527a8487d1138869e7c1c5c
Taylor Newill <tanewill@microsoft.com>
2016-09-29 08:29:57 -0400

    Update install-ccm.sh

--- RawStarCCMCluster/install-ccm.sh
+++ RawStarCCMCluster/install-ccm.sh
@@ -19,9 +19,15 @@ echo export INTELMPI_ROOT=/opt/intel/impi/5.1.3.181 >> /home/$USER/.bashrc
 echo export I_MPI_FABRICS=shm:dapl >> /home/$USER/.bashrc
 echo export I_MPI_DAPL_PROVIDER=ofa-v2-ib0 >> /home/$USER/.bashrc
 echo export I_MPI_ROOT=/opt/intel/compilers_and_libraries_2016.2.181/linux/mpi >> /home/$USER/.bashrc
-#echo export PATH=/mnt/scrat/ansys_inc/v172/fluent/bin:/opt/intel/impi/5.1.3.181/bin64:$PATH >> /home/$USER/.bashrc
+echo export PATH=/mnt/scratch/applications/STAR-CCM+11.02.010-R8/star/bin:/opt/intel/impi/5.1.3.181/bin64:$PATH >> /home/$USER/.bashrc
 echo export I_MPI_DYNAMIC_CONNECTION=0 >> /home/$USER/.bashrc
 
-source /mnt/scratch/INSTALLERS/starccm+_11.02.010/STAR-CCM+11.02.010_01_linux-x86_64-2.5_gnu4.8-r8.bin -i silent -DINSTALLDIR=/mnt/scratch/ -DNODOC=true -DINSTALLFLEX=false
-
+mkdir /mnt/scratch/applications
+source /mnt/scratch/INSTALLERS/starccm+_11.02.010/STAR-CCM+11.02.010_01_linux-x86_64-2.5_gnu4.8-r8.bin -i silent -DINSTALLDIR=/mnt/scratch/applications -DNODOC=true -DINSTALLFLEX=false
+rm -rf /mnt/scratch/STAR-CCM+11.02.010_01_linux-x86_64-r8.tar.gz
+rm /mnt/scratch/*.tgz
+mkdir /mnt/scratch/benchmark
+mv /mnt/scratch/*.sim /mnt/scratch/benchmark
+chown -R $USER:$USER /mnt/scratch
+chown -R $USER:$USER /mnt/resource/nfsshare
 
