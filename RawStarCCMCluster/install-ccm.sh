a79f696ee381781823a7c4932e7bca33c8470a4b
Taylor Newill <tanewill@microsoft.com>
2016-09-29 11:41:49 -0400

    Update install-ccm.sh

--- RawStarCCMCluster/install-ccm.sh
+++ RawStarCCMCluster/install-ccm.sh
@@ -13,8 +13,8 @@ cd /mnt/scratch/INSTALLERS/starccm+_11.02.010/
 
 #echo SERVER=1055@$LICIP > /mnt/nfsshare/ansys_inc/shared_files/licensing/ansyslmd.ini
 #echo ANSYSLI_SERVERS=2325@$LICIP >> /mnt/nfsshare/ansys_inc/shared_files/licensing/ansyslmd.ini
-#echo export FLUENT_HOSTNAME=$HOST >> /home/$USER/.bashrc
-
+echo export PODKey=$LICIP
+echo export CDLMD_LICENSE_FILE=1999@flex.cd-adapco.com >> /home/$USER/.bashrc
 echo export INTELMPI_ROOT=/opt/intel/impi/5.1.3.181 >> /home/$USER/.bashrc
 echo export I_MPI_FABRICS=shm:dapl >> /home/$USER/.bashrc
 echo export I_MPI_DAPL_PROVIDER=ofa-v2-ib0 >> /home/$USER/.bashrc
