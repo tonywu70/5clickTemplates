432b4d662e2c3852b07e3e0b8ad834608c44732f
Taylor Newill <tanewill@microsoft.com>
2016-10-17 20:59:58 +0100

    Update install-exa.sh

--- RawExaCluster/install-exa.sh
+++ RawExaCluster/install-exa.sh
@@ -5,36 +5,28 @@ HOST=`hostname`
 DOWN=$3
 echo $USER,$LICIP,$HOST,$DOWN
 
-sudo yum install -y compat-libstdc++-33.i686
-sudo yum install -y rsh
+yum install -y compat-libstdc++-33.i686
+#yum install -y rsh
 
-mkdir /mnt/scratch/applications
-mkdir /mnt/scratch/INSTALLERS
-mkdir /mnt/scratch/benchmark
+mkdir /mnt/resource/scratch/applications
+mkdir /mnt/resource/scratch/INSTALLERS
+mkdir /mnt/resource/scratch/benchmark
 
-wget -q http://azbenchmarkstorage.blob.core.windows.net/exabenchmarkstorage/.mpifile -O /mnt/scratch/benchmark/.mpifile
-wget -q http://azbenchmarkstorage.blob.core.windows.net/exabenchmarkstorage/powerflow-5.3c-linux.tar.gz -O /mnt/scratch/powerflow-5.3c-linux.tar.gz
-wget -q http://azbenchmarkstorage.blob.core.windows.net/cdadapcobenchmarkstorage/$DOWN -O /mnt/scratch/$DOWN
+wget -q http://azbenchmarkstorage.blob.core.windows.net/exabenchmarkstorage/.mpifile -O /mnt/resource/scratch/benchmark/.mpifile
+wget -q http://azbenchmarkstorage.blob.core.windows.net/exabenchmarkstorage/powerflow-5.3c-linux.tar.gz -O /mnt/resource/scratch/powerflow-5.3c-linux.tar.gz
+wget -q http://azbenchmarkstorage.blob.core.windows.net/cdadapcobenchmarkstorage/$DOWN -O /mnt/resource/scratch/$DOWN
 
-tar -xf /mnt/scratch/$DOWN -C /mnt/scratch
-tar -xzf /mnt/scratch/powerflow-5.3c-linux.tar.gz -C /mnt/scratch/INSTALLERS/
+tar -xf /mnt/resource/scratch/$DOWN -C /mnt/resource/scratch
+tar -xzf /mnt/resource/scratch/powerflow-5.3c-linux.tar.gz -C /mnt/resource/scratch/INSTALLERS/
 
-cd /mnt/scratch/INSTALLERS/starccm+_11.04.012/
+cd /mnt/resource/scratch/INSTALLERS/powerflow-5.3c
 
-echo export PODKey=$LICIP >> /home/$USER/.bashrc
-echo export CDLMD_LICENSE_FILE=1999@flex.cd-adapco.com >> /home/$USER/.bashrc
 echo export HOSTS=/home/$USER/bin/nodenames.txt
 echo export INTELMPI_ROOT=/opt/intel/impi/5.1.3.181 >> /home/$USER/.bashrc
 echo export I_MPI_FABRICS=shm:dapl >> /home/$USER/.bashrc
 echo export I_MPI_DAPL_PROVIDER=ofa-v2-ib0 >> /home/$USER/.bashrc
 echo export I_MPI_ROOT=/opt/intel/compilers_and_libraries_2016.2.181/linux/mpi >> /home/$USER/.bashrc
-echo export PATH=/mnt/scratch/applications/STAR-CCM+11.04.012-R8/star/bin:/opt/intel/impi/5.1.3.181/bin64:$PATH >> /home/$USER/.bashrc
 echo export I_MPI_DYNAMIC_CONNECTION=0 >> /home/$USER/.bashrc
-echo '/mnt/scratch/applications/STAR-CCM+11.04.012-R8/star/bin/starccm+ -np 8 -machinefile '$HOSTS' -power -podkey '$PODKey' -rsh ssh -mpi intel -cpubind bandwidth,v -mppflags " -ppn 8 -genv I_MPI_DAPL_PROVIDER=ofa-v2-ib0 -genv I_MPI_DAPL_UD=0 -genv I_MPI_DYNAMIC_CONNECTION=0" -batch runAndRecord.java /mnt/scratch/benchmark/*.sim' >> /mnt/scratch/benchmark/runccm_example.sh
 
-sh /mnt/scratch/INSTALLERS/starccm+_11.04.012/STAR-CCM+11.04.012_01_linux-x86_64-2.5_gnu4.8-r8.bin -i silent -DINSTALLDIR=/mnt/scratch/applications -DNODOC=true -DINSTALLFLEX=false
-rm -rf /mnt/scratch/STAR-CCM+11.04.010_01_linux-x86_64-r8.tar.gz
-rm /mnt/scratch/*.tgz
-mv /mnt/scratch/*.sim /mnt/scratch/benchmark
-chown -R $USER:$USER /mnt/scratch/*
+chown -R $USER:$USER /mnt/resource/scratch/*
 chown -R $USER:$USER /mnt/nfsshare
