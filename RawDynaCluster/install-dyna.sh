1749272626223316984758b90ddea01798742a5b
Taylor Newill <tanewill@microsoft.com>
2016-10-18 05:41:51 +0100

    Update install-dyna.sh

--- RawDynaCluster/install-dyna.sh
+++ RawDynaCluster/install-dyna.sh
@@ -12,5 +12,9 @@ mkdir /mnt/resource/scratch/applications
 mkdir /mnt/resource/scratch/INSTALLERS
 mkdir /mnt/resource/scratch/benchmark
 
-wget -q http://azbenchmarkstorage.blob.core.windows.net/dynabenchmarkstorage/ls-dyna_mpp_s_r7_1_2_95028_x64_redhat54_ifort131_sse2_intelmpi-413.tar.gz
-wget -q http://azbenchmarkstorage.blob.core.windows.net/dynabenchmarkstorage/neon_refined_revised.tar.gz
+wget -q http://azbenchmarkstorage.blob.core.windows.net/dynabenchmarkstorage/ls-dyna_mpp_s_r7_1_2_95028_x64_redhat54_ifort131_sse2_intelmpi-413.tar.gz -O /mnt/resource/scratch/INSTALLERS/ls-dyna_mpp_s_r7_1_2_95028_x64_redhat54_ifort131_sse2_intelmpi-413.tar.gz
+wget -q http://azbenchmarkstorage.blob.core.windows.net/dynabenchmarkstorage/neon_refined_revised.tar.gz -O /mnt/resource/scratch/benchmark/neon_refined_revised.tar.gz
+
+cd /mnt/resource/scratch/INSTALLERS/
+gunzip ls-dyna_mpp_s_r7_1_2_95028_x64_redhat54_ifort131_sse2_intelmpi-413.tar.gz
+tar -xf ls-dyna_mpp_s_r7_1_2_95028_x64_redhat54_ifort131_sse2_intelmpi-413.tar
