c7b1443edd80494ab84162346a177b87b04099c1
Taylor Newill <tanewill@microsoft.com>
2016-10-18 05:43:58 +0100

    Update cn-setup.sh

--- RawDynaCluster/cn-setup.sh
+++ RawDynaCluster/cn-setup.sh
@@ -18,7 +18,7 @@ systemctl start nfs-lock
 systemctl start nfs-idmap
 localip=`hostname -i | cut --delimiter='.' -f -3`
 echo "$IPPRE:/mnt/nfsshare    /mnt/nfsshare   nfs defaults 0 0" | tee -a /etc/fstab
-echo "$IPPRE:/mnt/resource/scratch    /mnt/scratch   nfs defaults 0 0" | tee -a /etc/fstab
+echo "$IPPRE:/mnt/resource/scratch    /mnt/resource/scratch   nfs defaults 0 0" | tee -a /etc/fstab
 showmount -e 10.0.0.4
 mount -a
 
