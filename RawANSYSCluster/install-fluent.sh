89241740a4f1eae68ed20ff46e35fa0f095b2f2d
Taylor Newill <tanewill@microsoft.com>
2016-10-22 17:21:00 -0400

    Update install-fluent.sh

--- RawANSYSCluster/install-fluent.sh
+++ RawANSYSCluster/install-fluent.sh
@@ -6,24 +6,26 @@ echo $USER,$LICIP,$HOST
 
 mkdir /mnt/resource/scratch
 mkdir /mnt/resource/scratch/INSTALLERS
+mkdir /mnt/resource/scratch/INSTALLERS/ANSYS
+wget -q http://azbenchmarkstorage.blob.core.windows.net/ansysbenchmarkstorage/ANSYS.tgz -o /mnt/resource/scratch/
 tar -xzf /mnt/resource/scratch/ANSYS.tgz -C /mnt/resource/scratch/INSTALLERS/
 
 cd /mnt/resource/scratch/INSTALLERS/ANSYS/
-mkdir -p /mnt/nfsshare/ansys_inc/shared_files/licensing/
+mkdir -p /mnt/resource/scratch/applications/ansys_inc/shared_files/licensing/
 
-echo SERVER=1055@$LICIP > /mnt/nfsshare/ansys_inc/shared_files/licensing/ansyslmd.ini
-echo ANSYSLI_SERVERS=2325@$LICIP >> /mnt/nfsshare/ansys_inc/shared_files/licensing/ansyslmd.ini
+echo SERVER=1055@$LICIP > /mnt/resource/scratch/applications/ansys_inc/shared_files/licensing/ansyslmd.ini
+echo ANSYSLI_SERVERS=2325@$LICIP >> /mnt/resource/scratch/applications/ansys_inc/shared_files/licensing/ansyslmd.ini
 
 echo export FLUENT_HOSTNAME=$HOST >> /home/$USER/.bashrc
 echo export INTELMPI_ROOT=/opt/intel/impi/5.1.3.181 >> /home/$USER/.bashrc
 echo export I_MPI_FABRICS=shm:dapl >> /home/$USER/.bashrc
 echo export I_MPI_DAPL_PROVIDER=ofa-v2-ib0 >> /home/$USER/.bashrc
 echo export I_MPI_ROOT=/opt/intel/compilers_and_libraries_2016.2.181/linux/mpi >> /home/$USER/.bashrc
-echo export PATH=/mnt/nfsshare/ansys_inc/v172/fluent/bin:/opt/intel/impi/5.1.3.181/bin64:$PATH >> /home/$USER/.bashrc
+echo export PATH=/mnt/resource/scratch/applications/ansys_inc/v172/fluent/bin:/opt/intel/impi/5.1.3.181/bin64:$PATH >> /home/$USER/.bashrc
 echo export I_MPI_DYNAMIC_CONNECTION=0 >> /home/$USER/.bashrc
 
-source /mnt/resource/scratch/INSTALLERS/ANSYS/INSTALL -silent -install_dir "/mnt/nfsshare/ansys_inc/" -fluent
-source /mnt/resource/scratch/INSTALLERS/ANSYS/INSTALL -silent -install_dir "/mnt/nfsshare/ansys_inc/" -cfx
+source /mnt/resource/scratch/INSTALLERS/ANSYS/INSTALL -silent -install_dir "/mnt/resource/scratch/applications/ansys_inc/" -fluent
+source /mnt/resource/scratch/INSTALLERS/ANSYS/INSTALL -silent -install_dir "/mnt/resource/scratch/applications/ansys_inc/" -cfx
 
 
 
