892709a066def8c1cdedde037e141f043d240e2f
Taylor Newill <tanewill@microsoft.com>
2016-11-01 02:02:47 -0700

    Update install-of.sh

--- RawOpenFOAMCluster/install-of.sh
+++ RawOpenFOAMCluster/install-of.sh
@@ -6,33 +6,32 @@ DOWN=$3
 echo $USER,$LICIP,$HOST,$DOWN
 mkdir /mnt/resource/scratch/
 mkdir /mnt/resource/scratch/applications
+mkdir /mnt/resource/scratch/applications/OpenFOAM
 mkdir /mnt/resource/scratch/INSTALLERS
 mkdir /mnt/resource/scratch/benchmark
 
-wget -q http://azbenchmarkstorage.blob.core.windows.net/cdadapcobenchmarkstorage/runAndRecord.java -O /mnt/resource/scratch/benchmark/runAndRecord.java
-wget -q http://azbenchmarkstorage.blob.core.windows.net/cdadapcobenchmarkstorage/STAR-CCM+11.04.012_01_linux-x86_64-r8.tar.gz -O /mnt/resource/scratch/STAR-CCM+11.04.012_01_linux-x86_64-r8.tar.gz
-wget -q http://azbenchmarkstorage.blob.core.windows.net/cdadapcobenchmarkstorage/$DOWN -O /mnt/resource/scratch/$DOWN
+wget -q http://tn0hpc0storage0east.blob.core.windows.net/builds/OpenFOAM_CentOS7_HPC.tgz -O /mnt/resource/scratch/OpenFOAM_CentOS7_HPC.tgz
+wget -q http://tn0hpc0storage0east.blob.core.windows.net/builds/OpenFOAM_CentOS7_HPC_libs.tgz -O /mnt/resource/scratch/OpenFOAM_CentOS7_HPC_libs.tgz
+wget -q http://tn0hpc0storage0east.blob.core.windows.net/builds/$DOWN -O /mnt/resource/scratch/benchmark/$DOWN
 
-tar -xf /mnt/resource/scratch/$DOWN -C /mnt/resource/scratch
-tar -xzf /mnt/resource/scratch/STAR-CCM+11.04.012_01_linux-x86_64-r8.tar.gz -C /mnt/resource/scratch/INSTALLERS/
+tar -xzf /mnt/resource/scratch/benchmark/$DOWN -C /mnt/resource/scratch/benchmark/
+tar -xzf /mnt/resource/scratch/OpenFOAM_CentOS7_HPC.tgz -C /mnt/resource/scratch/applications/OpenFOAM/
+tar -xzf /mnt/resource/scratch/OpenFOAM_CentOS7_HPC_libs.tgz -C /mnt/resource/scratch/applications/OpenFOAM/
 
-cd /mnt/resource/scratch/INSTALLERS/starccm+_11.04.012/
-
-echo export PODKey=$LICIP >> /home/$USER/.bashrc
-echo export CDLMD_LICENSE_FILE=1999@flex.cd-adapco.com >> /home/$USER/.bashrc
+		
+echo source /opt/intel/impi/5.1.3.181/bin64/mpivars.sh >> /home/$USER/.bashrc
 echo export HOSTS=/home/$USER/bin/nodenames.txt
 echo export INTELMPI_ROOT=/opt/intel/impi/5.1.3.181 >> /home/$USER/.bashrc
 echo export I_MPI_FABRICS=shm:dapl >> /home/$USER/.bashrc
 echo export I_MPI_DAPL_PROVIDER=ofa-v2-ib0 >> /home/$USER/.bashrc
-echo export I_MPI_ROOT=/opt/intel/compilers_and_libraries_2016.2.181/linux/mpi >> /home/$USER/.bashrc
-echo export PATH=/mnt/resource/scratch/applications/STAR-CCM+11.04.012-R8/star/bin:/opt/intel/impi/5.1.3.181/bin64:$PATH >> /home/$USER/.bashrc
 echo export I_MPI_DYNAMIC_CONNECTION=0 >> /home/$USER/.bashrc
-echo '/mnt/resource/scratch/applications/STAR-CCM+11.04.012-R8/star/bin/starccm+ -np 8 -machinefile '$HOSTS' -power -podkey '$PODKey' -rsh ssh -mpi intel -cpubind bandwidth,v -mppflags " -ppn 8 -genv I_MPI_DAPL_PROVIDER=ofa-v2-ib0 -genv I_MPI_DAPL_UD=0 -genv I_MPI_DYNAMIC_CONNECTION=0" -batch runAndRecord.java /mnt/resource/scratch/benchmark/*.sim' >> /mnt/resource/scratch/benchmark/runccm_example.sh
+echo export I_MPI_ROOT=/opt/intel/compilers_and_libraries_2016.2.181/linux/mpi >> /home/$USER/.bashrc
+echo export MPI_ROOT=$I_MPI_ROOT >> /home/$USER/.bashrc
+echo export LD_LIBRARY_PATH=/mnt/resource/scratch/applications/OpenFOAM/intel64_lin:$LD_LIBRARY_PATH >> /home/$USER/.bashrc
+echo export FOAM_INST_DIR=/mnt/resource/scratch/applications/OpenFOAM >> /home/$USER/.bashrc
+echo source /mnt/resource/scratch/applications/OpenFOAM/OpenFOAM-2.3.x/etc/bashrc >> /home/$USER/.bashrc
 
-sh /mnt/resource/scratch/INSTALLERS/starccm+_11.04.012/STAR-CCM+11.04.012_01_linux-x86_64-2.5_gnu4.8-r8.bin -i silent -DINSTALLDIR=/mnt/resource/scratch/applications -DNODOC=true -DINSTALLFLEX=false
-rm -rf /mnt/resource/scratch/STAR-CCM+11.04.010_01_linux-x86_64-r8.tar.gz
 rm /mnt/resource/scratch/*.tgz
-mv /mnt/resource/scratch/*.sim /mnt/resource/scratch/benchmark
 chown -R $USER:$USER /mnt/resource/scratch/*
 chown -R $USER:$USER /mnt/resource/nfsshare
 
