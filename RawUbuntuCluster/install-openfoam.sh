8f0ccabfe6f6ea5b330e79e93ac02f6a6bb7fdb2
Taylor Newill <tanewill@microsoft.com>
2017-04-01 08:54:11 -0500

    added raw ubuntu for OpenFOAM

---
+++ RawUbuntuCluster/install-openfoam.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo add-apt-repository http://dl.openfoam.org/ubuntu
+sudo sh -c "wget -O - http://dl.openfoam.org/gpg.key | apt-key add -"
+sudo apt-get update
+sudo apt-get -y install openfoam4
