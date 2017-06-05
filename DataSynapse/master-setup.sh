fe55c7f76190d9acf33bc193ade9d3e1a0fc0105
Taylor Newill <tanewill@microsoft.com>
2017-06-05 15:42:59 -0400

    updated java path

--- DataSynapse/master-setup.sh
+++ DataSynapse/master-setup.sh
@@ -11,8 +11,8 @@ npm install -g azure-cli
 cd /opt/
 #wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz"
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz"
-tar xzf jdk-8u121-linux-x64.tar.gz
-export JAVA_HOME=/opt/jdk1.8.0_121
+tar xzf jdk-8u131-linux-x64.tar.gz
+export JAVA_HOME=/opt/jdk1.8.0_131
 export PATH=$PATH:$JAVA_HOME/bin
 
 mkdir tibco
