802177192edffb75bcde463e69d4214a1b7eec20
tonywu70 <tony.wenyong@gmail.com>
2017-07-14 16:02:43 +0800

    Update hn-setup.sh

--- RawStarCCMCluster/hn-setup.sh
+++ RawStarCCMCluster/hn-setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+10#!/bin/bash
 USER=$1
 PASS=$2
 LICIP=$3
@@ -23,7 +23,7 @@ mkdir -p /mnt/nfsshare
 ln -s /opt/intel/impi/5.1.3.181/intel64/bin/ /opt/intel/impi/5.1.3.181/bin
 ln -s /opt/intel/impi/5.1.3.181/lib64/ /opt/intel/impi/5.1.3.181/lib
 
-wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
+wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-10.noarch.rpm
 
 rpm -ivh epel-release-7-9.noarch.rpm
 yum install -y -q nfs-utils sshpass nmap htop npm
